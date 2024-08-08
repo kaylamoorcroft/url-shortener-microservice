abca7ad806033e3372248573d0d901f61353aca3
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-08-08 17:39:37 +0000

    feat: add POST and GET requests for /api/shorturl
    feat: return original_url in json object
    
    feat: install mongoose and add as dependency to package.json

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+// for parsing body of post requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +23,13 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+// URL shortener microservice
+app.route('/api/shorturl')
+  .post((req, res) => {
+    console.log(req.body);
+    res.json({ original_url: req.body.url });})
+  .get((req, res) => res.json({ original_url: req.query.original_url }));
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
