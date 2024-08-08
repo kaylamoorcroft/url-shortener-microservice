173a3b6ff62b32ef977ad77f2c3ef4e9aee7b134
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-08-08 18:56:55 +0000

    feat: create methods to add new url to database and to find entry by id
    fix: createUrl() function name
    
    fix: new_url.save() syntax

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -24,10 +26,29 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// url schema and model
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({ original_url: String });
+const Url = mongoose.model("Url", urlSchema);
+
+// create and save url entry in database
+const createUrl = function(url, done) {
+  const new_url = new Url({ original_url: url });
+  new_url.save();
+  return new_url;
+};
+
+// find url entry by id
+const findUrlById = function(urlId, done) {
+  Url.findById(urlId, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
+};
+
 // URL shortener microservice
 app.route('/api/shorturl')
   .post((req, res) => {
-    console.log(req.body);
     res.json({ original_url: req.body.url });})
   .get((req, res) => res.json({ original_url: req.query.original_url }));
 
