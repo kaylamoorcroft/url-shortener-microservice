4e8059b13bc2d8252373c1f450d340878e6b3a43
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-08-08 21:09:50 +0000

    feat: add short_url key to JSON response that automatically increments
    feat: determine last short_url and increment when adding new entry
    
    feat: add short_url key to json response with corresponding value

--- index.js
+++ index.js
@@ -34,11 +34,24 @@ const urlSchema = new Schema({
 });
 const Url = mongoose.model("Url", urlSchema);
 
+// find next short_url id
+const findNextId = async () => {
+  const query = Url.find({}).sort({"short_url" : -1}).limit(1).exec();
+  const entries = await query;
+  // if no urls in database, return 1
+  if (!entries.length) return 1;
+  // otherwise, return next num
+  const nextId = entries[0].short_url + 1;
+  return nextId;
+};
+
 // create and save url entry in database
 const createUrl = async function(url) {
-  const new_url = new Url({ original_url: url });
-  await new_url.save();
-  return new_url;
+  const urlId = await findNextId();
+  console.log("new id is: " + urlId);
+  const newUrl = new Url({ original_url: url, short_url: urlId });
+  await newUrl.save();
+  return newUrl;
 };
 
 // find url entry by short_url
@@ -51,21 +64,28 @@ const findUrl = url => Url.findOne({original_url: url}).then(existing_url => exi
 app.route('/api/shorturl')
   .post((req, res) => {
     findUrl(req.body.url).then(existing_url => {
-    console.log(existing_url);
-    console.log("------");
-    // if url exists, get id
-    if (existing_url) {
-      console.log("url exists");
-      console.log("short_url: " + existing_url.short_url);
-    }
-    // otherwise, create new url and get id
-    else {
-      console.log("url doesn't exist");
-      createUrl(req.body.url).then(new_url => {
-        console.log(`new short_url for ${req.body.url}: ${new_url.short_url}`);
-      });
-    }
-    res.json({ original_url: req.body.url });});
+      console.log(existing_url);
+      console.log("------");
+      // if url exists
+      if (existing_url) {
+        // get id
+        const { original_url: url, short_url: id} = existing_url;
+        console.log("url exists:");
+        console.log("short_url: " + id);
+        // send json object
+        res.json({ original_url: url, short_url: id});
+      }
+      else {
+        console.log("url doesn't exist");
+        // create new url and get id
+        createUrl(req.body.url)
+        .then(({ original_url: url, short_url: id }) => {
+          console.log(`new short_url for ${url}: ${id}`);
+          // send json object
+          res.json({ original_url: url, short_url: id });
+        });
+      }
+    });
   });
     /*
   .get((req, res) => {
