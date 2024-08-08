58a09b3597790cfae75a38c7a3471494ea6d00bf
Kayla Moorcroft <kayla.moorcroft@icloud.com>
2024-08-08 19:09:12 +0000

    feat: create new database entry in POST method if url doesn't exist
    feat: create new database entry in POST method
    
    test: comment out GET request to /api/shorturl
    
    feat: only create new entry if url doesn't exist in POST request
    
    feat: create function to find db entry by url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const urlSchema = new Schema({ original_url: String });
 const Url = mongoose.model("Url", urlSchema);
 
 // create and save url entry in database
-const createUrl = function(url, done) {
+const createUrl = function(url) {
   const new_url = new Url({ original_url: url });
   new_url.save();
   return new_url;
@@ -46,11 +46,37 @@ const findUrlById = function(urlId, done) {
   });
 };
 
+// find url entry by original_url
+const findUrl = function(url) {
+  const existing_url = Url.findOne({original_url: url});
+  return existing_url;
+}
+
 // URL shortener microservice
 app.route('/api/shorturl')
   .post((req, res) => {
-    res.json({ original_url: req.body.url });})
-  .get((req, res) => res.json({ original_url: req.query.original_url }));
+    const existing_url = findUrl(req.body.url);
+    console.log(existing_url);
+    let id;
+    // if url exists, get id
+    if (existing_url) {
+      id = Number(existing_url._id);
+      console.log("short_url: " + id);
+    }
+    // otherwise, create new url and get id
+    else {
+      const new_url = createUrl(req.body.url);
+      console.log(new_url);
+      id = Number(new_url._id);
+      console.log("short_url: " + id);
+    }
+    res.json({ original_url: req.body.url });});
+    /*
+  .get((req, res) => {
+    const url = findUrl(req.query.original_url);
+    console.log("url: \n" + url);
+    res.json({ original_url: url.original_url});
+  });*/
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
